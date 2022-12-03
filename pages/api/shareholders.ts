9c30fe0c40217284ebf2a1ca9f6d3b01d4c9f1d1
Paul Le <paulle@Pauls-MacBook-Air.local>
2022-12-03 14:48:07 +1100

    Simple api to get all shareholders and total shareholders

--- pages/api/shareholders.ts
+++ pages/api/shareholders.ts
@@ -4,16 +4,23 @@ import type { NextApiHandler } from 'next';
 // This API endpoint will return all shareholders.
 // You might need to uplift this to perform better and fullfil the business requirements.
 const handler: NextApiHandler = async (req, res) => {
-  if (req.method !== 'GET') {
-    res.setHeader('Allow', ['GET']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+  try {
+    if (req.method !== 'GET') {
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
 
-    return;
+      return;
+    }
+
+    const [shareholders, totalShareholders] = await prisma.$transaction([
+      prisma.shareholder.findMany(),
+      prisma.shareholder.count(),
+    ]);
+
+    res.status(200).json({ shareholders, totalShareholders });
+  } catch (error) {
+    res.status(500).json({ error });
   }
-
-  const shareholders = await prisma.shareholder.findMany();
-
-  res.status(200).json(shareholders);
 };
 
 export default handler;

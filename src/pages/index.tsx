cc44cd2daf52d9b1745c82bd95596aae0a1cd953
Paul Le <paulle@Pauls-MacBook-Air.local>
2022-12-03 18:16:01 +1100

    Write up of action steps to complete tasks 1 and 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <h1>Home</h1>
     </div>
   );
 };
